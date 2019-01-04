54c4f2d4f4e10dff32412605a737b1ad42614ef1
illindima <illindima@gmail.com>
2019-01-04 11:48:13 +0200

    Inital

---
+++ app.js
@@ -0,0 +1,56 @@
+const LSFR = require('./lsfr')
+
+class App{
+    static run(){
+        const deg1 = 22
+        const deg2 = 21
+
+        let z1 = LSFR.generateImpulsSequence(deg1)
+
+
+        console.log(z1.join(''))
+
+        let s1 = LSFR.generateLSFR1(2 ** deg1 - 1,deg1).map((item) => +item)
+        console.log(s1.join(''))
+
+        let period1 = LSFR.getPeriod(s1,z1)
+
+        console.log(period1)
+
+        LSFR.getPolynomInfo(period1, 2 ** deg1 - 1)
+
+
+        let q1 = LSFR.generateLSFR1(period1,deg1).map((item) => +item)
+
+        LSFR.autoCorrelation(q1)
+
+
+        for(let i = 1; i < 3; i++){
+            console.log(LSFR.calculateKGramFrequency(q1,i))
+        }
+
+        console.log('-----')
+
+        let z2 = LSFR.generateImpulsSequence(deg2)
+        console.log(z2.join(''))
+
+        let s2 = LSFR.generateLSFR2(2 ** deg2 - 1,deg2).map((item) => +item)
+
+        let period2 = LSFR.getPeriod(s2,z2)
+
+        console.log(period2)
+
+        LSFR.getPolynomInfo(period2, 2 ** deg2 - 1)
+
+        let q2 = LSFR.generateLSFR1(period2,deg2).map((item) => +item)
+        LSFR.autoCorrelation(q2)
+
+        for(let i = 1; i < 3; i++){
+            console.log(LSFR.calculateKGramFrequency(q2,i))
+        }
+
+    }
+}
+
+
+App.run()
---
+++ lsfr.js
@@ -0,0 +1,118 @@
+const fs = require('fs')
+
+const generateImpulsSequence = polynomPow => (1 << polynomPow - 1).toString(2).split('').reverse()
+
+const generateLSFR1 = (iterations,n) => {
+    var rs = generateImpulsSequence(n)
+
+    for(let i = 0; i < iterations; i++){
+        rs.push(
+            rs[i+17] ^ rs[i + 15] ^ rs[i + 14] ^ rs[i + 13] ^ rs[i + 11] ^ rs[i + 8] ^ rs[i + 7] ^ rs[i + 6] ^ rs[i + 3] ^ rs[i + 2] ^ rs[i + 1] ^ rs[i]
+        )
+    }
+
+    return rs
+}
+
+const generateLSFR2 = (iterations,n) => {
+    var rs = generateImpulsSequence(n)
+
+    for(let i = 0; i < iterations; i++){
+         
+        rs.push(
+            rs[i+16] ^ rs[i + 15] ^ rs[i + 14] ^ rs[i + 13] ^ rs[i + 10] ^ rs[i + 9] ^ rs[i + 7] ^ rs[i + 2] ^ rs[i + 1] ^ rs[i]
+        )
+    }
+
+    return rs
+}
+const getPolynomInfo = (period,maxPeriod) => {
+    if(period === maxPeriod){
+        console.log("Примітивний, з чого слідує що він незвідний")
+    }
+    else if(maxPeriod % period == 0){
+        console.log("Непримітивний, можливо незвідний, бо період ділить максимальний період")
+    }
+    else{
+        console.log("Непримітивний, звідний")
+    }
+}
+
+
+const getPeriod = (sequence,state) => {
+    let period = 1, n = state.length
+
+    for(let i = 1; i < sequence.length; i++){
+        if(sequence.slice(i,i + n).join('') == state.join('')){
+            return period
+        }
+        else{
+            period++
+        }
+    }
+    return period
+}
+
+
+const autoCorrelation = (sequence, period = 10) => {
+    let autoCorrelation = {}
+
+    for(let d = 0; d < period + 1; d++){
+        let shifted = Array.prototype.concat(
+            sequence.slice(sequence.length - d,sequence.length),sequence.slice(0,sequence.length - d)
+        )
+       
+        autoCorrelation[d] = 0
+        for(let i = 0; i < sequence.length; i++){
+            if(sequence[i] != shifted[i]){
+                autoCorrelation[d] += 1
+            }
+        }
+    }
+
+    for(let item in autoCorrelation){
+       console.log(`d = ${item}: ${autoCorrelation[item]}`)
+    }
+}
+
+
+const calculateKGramFrequency = (sequence,step) => {
+   
+    let y = {};
+    let count = 0
+    for(let i = 0; i < sequence.length - 1; i += step){
+        if(y[sequence.slice(i,i + step).join('')]){
+            continue
+        }
+        else{
+            y[sequence.slice(i,i + step).join('')] = 0
+            count++;
+        }
+    }
+    for(let i = 0; i < sequence.length - 1; i += step){
+        let curKGram = sequence.slice(i,i + step).join('')
+        y[curKGram] += 1 
+    }
+    for(let i in y){
+        y[i] = y[i] / count
+    }
+
+    return y
+}
+
+
+
+
+
+
+
+module.exports = {
+    autoCorrelation: autoCorrelation,
+    calculateKGramFrequency: calculateKGramFrequency,
+    getPeriod: getPeriod,
+    generateImpulsSequence: generateImpulsSequence,
+    generateLSFR1: generateLSFR1,
+    generateLSFR2: generateLSFR2,
+    getPolynomInfo: getPolynomInfo,
+
+}
